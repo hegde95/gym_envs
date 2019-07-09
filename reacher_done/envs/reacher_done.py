0236192683892ec1b3d42c77171cfdf064faaa33
Gautam Salhotra <gautams3@users.noreply.github.com>
2019-07-09 14:27:51 -0700

    Rewards: Re-adding action
    Re-adding reduced penalty for excessive action.
    Coeff of action penalty should be a hyperparam

--- reacher_done/envs/reacher_done.py
+++ reacher_done/envs/reacher_done.py
@@ -14,16 +14,14 @@ class ReacherDoneEnv(ReacherEnv):
     vec = self.get_body_com("fingertip")-self.get_body_com("target")
     dist = np.linalg.norm(vec)
     reward_dist = - dist
-    reward_ctrl = 0 # - np.square(action).sum()
+    reward_ctrl = - 0.3 * np.square(action).sum()
     reward_time = -0.04
     done = dist < 0.04 # done if it's close enough
     done_reward = 2
     reward = reward_dist + reward_ctrl + reward_time + done*done_reward
     ob = self._get_obs()
-    # print("Dist ", dist)
-    if done:
-        print("Done!")
-    return ob, reward, done, dict(reward_dist=reward_dist, reward_ctrl=reward_ctrl)
+    info = dict(reward_dist=reward_dist, reward_ctrl=reward_ctrl, dist=dist)
+    return ob, reward, done, info
   
 #   def reset(self):
     # super().reset()

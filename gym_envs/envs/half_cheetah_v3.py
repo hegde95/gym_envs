e8f0475299feb3a6f1049fa7e1b8a4b4e6882e51
Gautam Salhotra <gautams3@users.noreply.github.com>
2021-02-03 15:13:16 -0800

    bugfix to allow deepcopy() on halfcheetah

--- gym_envs/envs/half_cheetah_v3.py
+++ gym_envs/envs/half_cheetah_v3.py
@@ -1,44 +1,22 @@
 import gym
 from gym.envs.mujoco.half_cheetah_v3 import HalfCheetahEnv
-from gym.utils.ezpickle import EzPickle
 import os
 
 
-class HalfCheetahSoftEnv(HalfCheetahEnv, EzPickle):
+class HalfCheetahSoftEnv(HalfCheetahEnv):
 
-  # HalfCheetah-v3 but with a different xml file
-  def __init__(self):
-    EzPickle.__init__(**locals())
-    curr_dir = os.path.dirname(os.path.realpath(__file__))
-    xml_path = os.path.join(curr_dir, 'assets', 'half_cheetah_soft.xml')
-    super(HalfCheetahSoftEnv, self).__init__(xml_file=xml_path)
+    # HalfCheetah-v3 but with a different xml file
+    def __init__(self):
+        curr_dir = os.path.dirname(os.path.realpath(__file__))
+        xml_path = os.path.join(curr_dir, 'assets', 'half_cheetah_soft.xml')
+        super(HalfCheetahSoftEnv, self).__init__(xml_file=xml_path)
 
-  # def step(self, action):
-  #   action = np.clip(action, self.action_space.low, self.action_space.high)
-  #   self._set_action(action)
-  #   self.sim.step()
-  #   self._step_callback()
-  #   obs = self._get_obs()
+    # gym uses Ezpickle for envs that uses C/C++ (like mujoco and atari envs)
+    # These functions are for pickling and unpickling
+    # To allow you to use deepcopy()
+    def __getstate__(self):
+        return {}  # no new variables to store from this derived class
 
-  #   done = self._is_success(obs['achieved_goal'], self.goal)
-  #   info = {
-  #       # does not include done from TimeLimit (episode completion)
-  #       'is_success': done,
-  #       'dist': goal_distance(obs['achieved_goal'], self.goal)
-  #   }
-
-  #   reward = self.compute_reward(obs['achieved_goal'], self.goal, info)
-
-  #   # Time penalty to encourage faster reaching
-  #   reward_time = -0.1  # TODO: Make tweakable hyperparam
-  #   reward = reward + reward_time
-  #   return obs, reward, done, info
-
-  # def compute_reward(self, achieved_goal, goal, info):
-  #   ...
-  # def reset(self):
-  #   ...
-  # def render(self, mode='human'):
-  #   ...
-  # def close(self):
-  #   ...
+    def __setstate__(self, d):
+        out = type(self)()  # call constructor with no other arguments
+        self.__dict__.update(out.__dict__)

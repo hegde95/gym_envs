3fbcb68b74debf77bb711f69e3700b19919b6321
Gautam Salhotra <gautams3@users.noreply.github.com>
2021-02-21 14:38:15 -0800

    add ability to specify custom xml
    * remove goal joints (it's a stationary visual object)

--- gym_envs/envs/half_cheetah_v3.py
+++ gym_envs/envs/half_cheetah_v3.py
@@ -2,22 +2,27 @@ import gym
 from gym.envs.mujoco.half_cheetah_v3 import HalfCheetahEnv
 import os
 
-
+# HalfCheetah-v3 but with a different xml file
 class HalfCheetahSoftEnv(HalfCheetahEnv):
+  _xml_path:str
 
-    # HalfCheetah-v3 but with a different xml file
-    def __init__(self):
-        curr_dir = os.path.dirname(os.path.realpath(__file__))
-        xml_path = os.path.join(curr_dir, 'assets', 'half_cheetah_soft.xml')
-        super(HalfCheetahSoftEnv, self).__init__(
-            xml_file=xml_path, reset_noise_scale=0.)
+  def __init__(self, xml_path = None):
+    if xml_path is None:
+      curr_dir = os.path.dirname(os.path.realpath(__file__))
+      self._xml_path = os.path.join(curr_dir, 'assets', 'half_cheetah_soft.xml')
+    else:
+      self._xml_path = xml_path
 
-    # gym uses Ezpickle for envs that uses C/C++ (like mujoco and atari envs)
-    # These functions are for pickling and unpickling
-    # To allow you to use deepcopy()
-    def __getstate__(self):
-        return {}  # no new variables to store from this derived class
+    super(HalfCheetahSoftEnv, self).__init__(
+      xml_file=self._xml_path, reset_noise_scale=0.)
 
-    def __setstate__(self, d):
-        out = type(self)()  # call constructor with no other arguments
-        self.__dict__.update(out.__dict__)
+  # gym uses Ezpickle for envs that uses C/C++ (like mujoco and atari envs)
+  # These functions are for pickling and unpickling
+  # To allow you to use deepcopy()
+  def __getstate__(self):
+    return {"_xml_path": self._xml_path}
+
+  def __setstate__(self, d):
+    # out = type(self)()  # call constructor with no other arguments
+    out = type(self)(*d["_xml_path"])
+    self.__dict__.update(out.__dict__)
